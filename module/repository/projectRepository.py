7b37f5f369c1b86b222bd896f5209a6edf9ca0ae
TikaWorld <tikaworld0416@gmail.com>
2019-06-13 12:29:50 +0900

    Implement getProject method

--- module/repository/projectRepository.py
+++ module/repository/projectRepository.py
@@ -3,17 +3,22 @@ import pymysql
 
 class ProjectRepository:
     def __init__(self):
-        self.url = ""
+        self.url = "localhost"
+        self.port = 3306
+        self.user = "root"
+        self.password = ""
+        self.db = "leepository"
+        self.table = "projects"
 
     def getProject(self, project):
-        query = "SELECT * FROM " + self.table + " WHERE name = " + project
+        query = "SELECT * FROM %s WHERE projectName = '%s'" % (self.table, project)
         return self.transaction(query)
 
     def addProject(self):
         return NotImplemented
 
     def transaction(self, query):
-        db = pymysql.connect()
+        db = pymysql.connect(host=self.url, port=self.port, user=self.user, passwd=self.password, db=self.db)
         try:
             with db.cursor() as cursor:
                 cursor.execute(query)
--- module/resource/project.py
+++ module/resource/project.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from module.projectRepository import ProjectRepository
+from module.repository.projectRepository import ProjectRepository
 
 
 class Project(Resource):
